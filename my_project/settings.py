b3958494937bfdc5e54b3a84eedd17f191b8078c
JorgenDIF <jorgenjonsson80@gmail.com>
2024-03-09 12:37:44 +0100

    Update project settings and requirements

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY ="django-insecure-3y8pxv9_4!1oifow&zsr4jb)52_i+=m57i4*4df#gb!f)+^byj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
